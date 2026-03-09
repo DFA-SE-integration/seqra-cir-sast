3817b94b11ed439ed984932e25511f58a8b9fea0
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:31:57 +0300

    seqra-utils: update

--- seqra-utils/seqra-jvm-util/build.gradle.kts
+++ seqra-utils/seqra-jvm-util/build.gradle.kts
@@ -2,9 +2,11 @@ import org.seqra.common.KotlinDependency
 import SeqraIrDependency.seqra_ir_api_jvm
 import SeqraIrDependency.seqra_ir_approximations
 import SeqraIrDependency.seqra_ir_core
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
     id("kotlin-conventions")
+    `maven-publish`
 }
 
 dependencies {
@@ -15,4 +17,18 @@ dependencies {
     implementation(seqra_ir_approximations)
 
     implementation(KotlinDependency.Libs.reflect)
+}
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra.utils"
+version = rootProperties.getProperty("seqraUtilVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("seqra-jvm-util") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
