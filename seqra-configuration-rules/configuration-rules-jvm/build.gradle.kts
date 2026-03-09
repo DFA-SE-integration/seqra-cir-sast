6328866d580f3a6b95c28d68bcd4c7b508766eb3
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:36:31 +0300

    seqra-configuration-rules: fix to build in monorepo

--- seqra-configuration-rules/configuration-rules-jvm/build.gradle.kts
+++ seqra-configuration-rules/configuration-rules-jvm/build.gradle.kts
@@ -1,10 +1,12 @@
 import SeqraIrDependency.seqra_ir_api_jvm
 import SeqraIrDependency.seqra_ir_core
 import org.seqra.common.KotlinDependency
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
     id("kotlin-conventions")
     kotlinSerialization()
+    `maven-publish`
 }
 
 dependencies {
@@ -15,4 +17,18 @@ dependencies {
 
     implementation(KotlinDependency.Libs.kotlinx_serialization_core)
     implementation(KotlinDependency.Libs.kaml)
+}
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra.configuration"
+version = rootProperties.getProperty("seqraConfigRulesVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("rules-jvm") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
