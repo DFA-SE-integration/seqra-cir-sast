bd73cc180c834eaa18dc2f17adc277ff3c7b3cdc
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:29:02 +0300

    seqra-project-model: fix to build in monorepo

--- seqra-project-model/build.gradle.kts
+++ seqra-project-model/build.gradle.kts
@@ -1,11 +1,28 @@
 import org.seqra.common.KotlinDependency
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
     id("kotlin-conventions")
     kotlinSerialization()
 }
 
+val kotlinVersion = "2.0.21"
+
 dependencies {
     implementation(KotlinDependency.Libs.kotlinx_serialization_core)
     implementation(KotlinDependency.Libs.kaml)
+}
+
+val rootProperties = layout.projectDirectory.file("gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra.project"
+version = rootProperties.getProperty("seqraProjectVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("root") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
