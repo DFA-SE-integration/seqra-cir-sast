45222ce63123f7d8068ac627d244868f7a541342
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:25:44 +0300

    seqra-ir: update

--- seqra-ir/seqra-ir-api-common/build.gradle.kts
+++ seqra-ir/seqra-ir-api-common/build.gradle.kts
@@ -1,3 +1,20 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 plugins {
     id("kotlin-conventions")
+    `maven-publish`
+}
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra"
+version = rootProperties.getProperty("seqraIrVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("api-common") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
