21e9c4e5af2be6e519458f08affeb82d3cb8fdbb
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:39:41 +0300

    seqra-sast-test-util: fix to build in monorepo

--- seqra-sast-test-util/build.gradle.kts
+++ seqra-sast-test-util/build.gradle.kts
@@ -1,3 +1,22 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 plugins {
     id("kotlin-conventions")
+    `maven-publish`
+}
+
+val kotlinVersion = "2.1.0"
+
+val rootProperties = layout.projectDirectory.file("gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra"
+version = rootProperties.getProperty("seqraBuildVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("root") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
