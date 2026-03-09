37e304a3afd6fcdfc66e4a3fe033cdbb3e90e58d
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:31:57 +0300

    seqra-utils: fix to build in monorepo

--- seqra-utils/common-util/build.gradle.kts
+++ seqra-utils/common-util/build.gradle.kts
@@ -1,3 +1,20 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 plugins {
     id("kotlin-conventions")
+    `maven-publish`
+}
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+
+group = "org.seqra.utils"
+version = rootProperties.getProperty("seqraUtilVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("common-util") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
