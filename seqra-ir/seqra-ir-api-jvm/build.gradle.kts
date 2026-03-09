cfbc34b80810b133a4e4bb8fd07c83d5e25bd9b5
dmittrey <dmitryzubahin@gmail.com>
2026-03-09 18:25:44 +0300

    seqra-ir: fix to build in monorepo

--- seqra-ir/seqra-ir-api-jvm/build.gradle.kts
+++ seqra-ir/seqra-ir-api-jvm/build.gradle.kts
@@ -1,8 +1,12 @@
 import org.seqra.common.KotlinDependency
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
     id("kotlin-conventions")
+    `maven-publish`
 }
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
 
 dependencies {
     api(project(":seqra-ir-api-common"))
@@ -14,4 +18,16 @@ dependencies {
     api(Libs.asm_util)
 
     api(KotlinDependency.Libs.kotlinx_coroutines_core)
+}
+
+group = "org.seqra"
+version = rootProperties.getProperty("seqraIrVersion")
+
+publishing {
+    publications {
+        create<MavenPublication>("api-jvm") {
+            from(components["java"])
+            tasks.findByName("kotlinSourcesJar")?.let { artifact(it) }
+        }
+    }
 }
