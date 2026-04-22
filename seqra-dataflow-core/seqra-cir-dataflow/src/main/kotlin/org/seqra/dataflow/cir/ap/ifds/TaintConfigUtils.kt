f67e9dfdb9728c6e0bfbf73d91864e80949c9afa
dmittrey <dmitryzubahin@gmail.com>
2026-04-22 19:42:16 +0300

    AS_IS: seqra-cir-dataflow: copy from jvm-dataflow
    CIRMarkAwareConditionExpr:
    - as is org/seqra/dataflow/jvm/ap/ifds/JIRMarkAwareConditionExpr.kt
    CIRMarkAwareConditionRewriter:
    - as is org/seqra/dataflow/jvm/ap/ifds/JIRMarkAwareConditionRewriter.kt
    AccessPathCreationUtils:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/AccessPathCreationUtils.kt
    PositionAccess:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/PositionAccess.kt
    FactReaderUtils:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/FactReaderUtils.kt
    FactReader:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/FactReader.kt
    TaintEvaluator:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/TaintEvaluator.kt
    CIRFactAwareConditionEvaluator:
    - as is org/seqra/dataflow/jvm/ap/ifds/JIRFactAwareConditionEvaluator.kt
    CIRSimpleFactAwareConditionEvaluator:
    - as is org/seqra/dataflow/jvm/ap/ifds/JIRSimpleFactAwareConditionEvaluator.kt
    TaintConfigUtils:
    - as is org/seqra/dataflow/jvm/ap/ifds/TaintConfigUtils.kt
    CIRTaintRulesProvider:
    - as is org/seqra/dataflow/jvm/ap/ifds/taint/TaintRulesProvider.kt

---
+++ seqra-dataflow-core/seqra-cir-dataflow/src/main/kotlin/org/seqra/dataflow/cir/ap/ifds/TaintConfigUtils.kt
@@ -0,0 +1,223 @@
+package org.seqra.dataflow.cir.ap.ifds
+
+import org.seqra.dataflow.ap.ifds.ExclusionSet
+import org.seqra.dataflow.ap.ifds.access.ApManager
+import org.seqra.dataflow.ap.ifds.access.InitialFactAp
+import org.seqra.dataflow.ap.ifds.taint.TaintSinkTracker.FactWithPreconditions
+import org.seqra.dataflow.cir.ap.ifds.taint.CIRTaintRulesProvider
+import org.seqra.dataflow.cir.ap.ifds.taint.ConditionEvaluator
+import org.seqra.dataflow.cir.ap.ifds.taint.FactReader
+import org.seqra.dataflow.cir.ap.ifds.taint.FinalFactReader
+import org.seqra.dataflow.cir.ap.ifds.taint.InitialFactReader
+import org.seqra.dataflow.cir.ap.ifds.taint.PassActionEvaluator
+import org.seqra.dataflow.cir.ap.ifds.taint.SourceActionEvaluator
+import org.seqra.dataflow.cir.ap.ifds.taint.TaintCleanActionEvaluator
+import org.seqra.dataflow.configuration.core.Action
+import org.seqra.dataflow.configuration.core.AssignMark
+import org.seqra.dataflow.configuration.core.Condition
+import org.seqra.dataflow.configuration.core.CopyAllMarks
+import org.seqra.dataflow.configuration.core.CopyMark
+import org.seqra.dataflow.configuration.core.RemoveAllMarks
+import org.seqra.dataflow.configuration.core.RemoveMark
+import org.seqra.dataflow.configuration.core.TaintConfigurationItem
+import org.seqra.dataflow.configuration.core.TaintEntryPointSource
+import org.seqra.ir.api.common.CommonMethod
+import org.seqra.ir.api.common.cfg.CommonInst
+import org.seqra.util.Maybe
+import org.seqra.util.maybeFlatMap
+
+object TaintConfigUtils {
+    fun sinkRules(config: CIRTaintRulesProvider, method: CommonMethod, statement: CommonInst) =
+        config.sinkRulesForMethod(method, statement)
+
+    fun <T> applyEntryPointConfig(
+        config: CIRTaintRulesProvider,
+        method: CommonMethod,
+        conditionEvaluator: ConditionEvaluator<Boolean>,
+        taintActionEvaluator: SourceActionEvaluator<T>
+    ) = applyAssignMark<TaintEntryPointSource, T>(
+        config.entryPointRulesForMethod(method), conditionEvaluator, taintActionEvaluator,
+        TaintEntryPointSource::condition, TaintEntryPointSource::actionsAfter
+    )
+
+    private inline fun <reified T : TaintConfigurationItem, R> applyAssignMark(
+        rules: Iterable<T>,
+        conditionEvaluator: ConditionEvaluator<Boolean>,
+        taintActionEvaluator: SourceActionEvaluator<R>,
+        condition: (T) -> Condition,
+        actionsAfter: (T) -> List<Action>
+    ): Maybe<List<R>> = rules
+        .filter { conditionEvaluator.eval(condition(it)) }
+        .maybeFlatMap { item ->
+            actionsAfter(item)
+                .filterIsInstance<AssignMark>()
+                .maybeFlatMap { taintActionEvaluator.evaluate(item, it) }
+        }
+
+    fun <T> applyPassThrough(
+        config: CIRTaintRulesProvider,
+        method: CommonMethod,
+        statement: CommonInst,
+        conditionEvaluator: ConditionEvaluator<Boolean>,
+        taintActionEvaluator: PassActionEvaluator<T>
+    ): Maybe<List<T>> =
+        config.passTroughRulesForMethod(method, statement)
+            .filter { conditionEvaluator.eval(it.condition) }
+            .maybeFlatMap { item ->
+                item.actionsAfter.maybeFlatMap {
+                    when (it) {
+                        is CopyMark -> taintActionEvaluator.evaluate(item, it)
+                        is CopyAllMarks -> taintActionEvaluator.evaluate(item, it)
+                        else -> Maybe.none()
+                    }
+                }
+            }
+
+    fun applyCleaner(
+        config: CIRTaintRulesProvider,
+        method: CommonMethod,
+        statement: CommonInst,
+        initialFact: FinalFactReader?,
+        conditionEvaluator: ConditionEvaluator<Boolean>,
+        taintActionEvaluator: TaintCleanActionEvaluator
+    ): FinalFactReader? =
+        config.cleanerRulesForMethod(method, statement)
+            .filter { conditionEvaluator.eval(it.condition) }
+            .fold(initialFact) { startFact, rule ->
+                rule.actionsAfter.fold(startFact) { fact, action ->
+                    when (action) {
+                        is RemoveMark -> taintActionEvaluator.evaluate(fact, action)
+                        is RemoveAllMarks -> taintActionEvaluator.evaluate(fact, action)
+                        else -> fact
+                    }
+                }
+            }
+
+    inline fun <T : TaintConfigurationItem> List<T>.applyRuleWithAssumptions(
+        apManager: ApManager,
+        conditionRewriter: CIRMarkAwareConditionRewriter,
+        conditionFactReaders: List<FactReader>,
+        condition: T.() -> Condition,
+        storeAssumptions: (T, Map<InitialFactAp, Set<InitialFactAp>>) -> Unit,
+        currentAssumptions: (T) -> Set<InitialFactAp>,
+        applyRule: (T, List<InitialFactAp>) -> Unit,
+    ) {
+        applyRuleWithAssumptions(
+            apManager = apManager,
+            conditionRewriter = conditionRewriter,
+            initialFacts = emptySet(),
+            conditionFactReaders = conditionFactReaders,
+            condition = condition,
+            storeAssumptions = storeAssumptions,
+            currentAssumptions = currentAssumptions,
+            currentAssumptionPreconditions = { _, facts ->
+                facts.map { FactWithPreconditions(it, emptyList()) }
+            },
+            applyRule = applyRule,
+            applyRuleWithAssumptions = { rule, facts ->
+                applyRule(rule, facts.map { it.fact })
+            }
+        )
+    }
+
+    inline fun <T : TaintConfigurationItem> List<T>.applyRuleWithAssumptions(
+        apManager: ApManager,
+        conditionRewriter: CIRMarkAwareConditionRewriter,
+        initialFacts: Set<InitialFactAp>,
+        conditionFactReaders: List<FactReader>,
+        condition: T.() -> Condition,
+        storeAssumptions: (T, Map<InitialFactAp, Set<InitialFactAp>>) -> Unit,
+        currentAssumptions: (T) -> Set<InitialFactAp>,
+        currentAssumptionPreconditions: (T, List<InitialFactAp>) -> List<FactWithPreconditions>,
+        applyRule: (T, List<InitialFactAp>) -> Unit,
+        applyRuleWithAssumptions: (T, List<FactWithPreconditions>) -> Unit
+    ) {
+        val conditionEvaluator = CIRFactAwareConditionEvaluator(conditionFactReaders)
+
+        for (rule in this) {
+            val ruleCondition = rule.condition()
+
+            val simplifiedCondition = conditionRewriter.rewrite(ruleCondition)
+            val conditionExpr = when {
+                simplifiedCondition.isFalse -> continue
+                simplifiedCondition.isTrue -> {
+                    applyRule(rule, emptyList())
+                    continue
+                }
+                else -> simplifiedCondition.expr
+            }
+
+            val ruleApplicable = conditionEvaluator.evalWithAssumptionsCheck(conditionExpr)
+
+            if (ruleApplicable) {
+                applyRule(rule, conditionEvaluator.facts())
+                continue
+            }
+
+            // no evaluated taint marks
+            val assumptionExpr = conditionEvaluator.assumptionExpr() ?: continue
+
+            val facts = conditionEvaluator.facts()
+            val factPrecondition = initialFacts.mapTo(hashSetOf()) {
+                it.replaceExclusions(ExclusionSet.Universe)
+            }.ifEmpty { emptySet() }
+
+            val newAssumptions = facts.associateWith { factPrecondition }
+            storeAssumptions(rule, newAssumptions)
+
+            val assumptions = currentAssumptions(rule)
+            val assumptionReaders = assumptions.map { InitialFactReader(it, apManager) }
+
+            val conditionEvaluatorWithAssumptions = CIRFactAwareConditionEvaluator(assumptionReaders)
+            if (!conditionEvaluatorWithAssumptions.evalWithAssumptionsCheck(assumptionExpr)) {
+                continue
+            }
+
+            val currentFactPreconditions = facts.map { FactWithPreconditions(it, listOf(factPrecondition)) }
+
+            val assumedFacts = conditionEvaluatorWithAssumptions.facts()
+
+            if (assumedFacts.size == 1) {
+                addRuleWithAssumption(
+                    currentAssumptionPreconditions,
+                    applyRuleWithAssumptions,
+                    rule,
+                    assumedFacts,
+                    currentFactPreconditions
+                )
+                continue
+            }
+
+            check(assumedFacts.size > 1) { "Multiple assumptions expected" }
+
+            val assumptionExprDnf = assumptionExpr.explodeToDNF().distinct()
+            for (cube in assumptionExprDnf) {
+                val expr = CIRMarkAwareConditionExpr.And(cube.literals.toTypedArray())
+                if (!conditionEvaluatorWithAssumptions.evalWithAssumptionsCheck(expr)) {
+                    continue
+                }
+
+                val cubeAssumedFacts = conditionEvaluatorWithAssumptions.facts()
+                addRuleWithAssumption(
+                    currentAssumptionPreconditions,
+                    applyRuleWithAssumptions,
+                    rule,
+                    cubeAssumedFacts,
+                    currentFactPreconditions
+                )
+            }
+        }
+    }
+
+    inline fun <T : TaintConfigurationItem> addRuleWithAssumption(
+        currentAssumptionPreconditions: (T, List<InitialFactAp>) -> List<FactWithPreconditions>,
+        applyRuleWithAssumptions: (T, List<FactWithPreconditions>) -> Unit,
+        rule: T,
+        assumedFacts: List<InitialFactAp>,
+        currentFactPreconditions: List<FactWithPreconditions>,
+    ) {
+        val assumedFactsPreconditions = currentAssumptionPreconditions(rule, assumedFacts)
+        val allFacts = assumedFactsPreconditions + currentFactPreconditions
+        applyRuleWithAssumptions(rule, allFacts)
+    }
+}
---
+++ seqra-dataflow-core/seqra-cir-dataflow/src/test/kotlin/org/seqra/dataflow/cir/ap/ifds/CIRMarkAwareConditionRewriterTest.kt
@@ -0,0 +1,199 @@
+package org.seqra.dataflow.cir.ap.ifds
+
+import org.seqra.dataflow.configuration.core.And
+import org.seqra.dataflow.configuration.core.Argument
+import org.seqra.dataflow.configuration.core.ConstantEq
+import org.seqra.dataflow.configuration.core.ConstantIntValue
+import org.seqra.dataflow.configuration.core.ContainsMark
+import org.seqra.dataflow.configuration.core.Not
+import org.seqra.dataflow.configuration.core.TaintMark
+import org.seqra.ir.api.cir.CIRClasspath
+import org.seqra.ir.api.cir.CIRClasspathFeature
+import org.seqra.ir.api.cir.CIRDatabase
+import org.seqra.ir.api.cir.RegisteredLocation
+import org.seqra.ir.api.cir.cfg.CIRAssignInst
+import org.seqra.ir.api.cir.cfg.CIRBlockList
+import org.seqra.ir.api.cir.cfg.CIRCallingConv
+import org.seqra.ir.api.cir.cfg.CIRConstantOpExpr
+import org.seqra.ir.api.cir.cfg.CIRExtraFuncAttributesAttr
+import org.seqra.ir.api.cir.cfg.CIRFuncOp
+import org.seqra.ir.api.cir.cfg.CIRFunction
+import org.seqra.ir.api.cir.cfg.CIRFunctionID
+import org.seqra.ir.api.cir.cfg.CIRFunctionParameter
+import org.seqra.ir.api.cir.cfg.CIRGlobalID
+import org.seqra.ir.api.cir.cfg.CIRGlobalLinkageKind
+import org.seqra.ir.api.cir.cfg.CIRInst
+import org.seqra.ir.api.cir.cfg.CIRInstLocation
+import org.seqra.ir.api.cir.cfg.CIRIntAttr
+import org.seqra.ir.api.cir.cfg.CIRVisibilityAttr
+import org.seqra.ir.api.cir.cfg.CIRVisibilityKind
+import org.seqra.ir.api.cir.cfg.MLIRDictionaryAttr
+import org.seqra.ir.api.cir.cfg.MLIROpID
+import org.seqra.ir.api.cir.cfg.MLIROpValue
+import org.seqra.ir.api.cir.cfg.MLIRModuleID
+import org.seqra.ir.api.cir.cfg.MLIRStringAttr
+import org.seqra.ir.api.cir.cfg.MLIRTypeID
+import org.seqra.ir.api.cir.cfg.MLIRUnknownLoc
+import org.seqra.ir.api.cir.cfg.MLIRValue
+import org.seqra.util.Maybe
+import java.math.BigInteger
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class CIRMarkAwareConditionRewriterTest {
+
+    private val moduleId = MLIRModuleID("test-mod")
+    private val voidTy = MLIRTypeID(moduleId, "void")
+    private val intTy = MLIRTypeID(moduleId, "i32")
+    private val extraAttrs = CIRExtraFuncAttributesAttr(MLIRDictionaryAttr(arrayListOf()))
+
+    private inner class StubClasspath : CIRClasspath {
+        override val db: CIRDatabase get() = error("unused")
+        override val registeredLocations: List<RegisteredLocation> = emptyList()
+        override val registeredLocationIds: Set<Long> = emptySet()
+        override val features: List<CIRClasspathFeature> = emptyList()
+        override val moduleNames: List<String> = listOf(moduleId.id)
+        override fun findFunctionOrNull(functionID: CIRFunctionID): CIRFunction? = null
+        override fun findFunctionBySymbolName(symbolName: String): CIRFunction? = null
+        override fun findTypeOrNull(typeID: MLIRTypeID) = null
+        override fun findGlobalOrNull(globalID: CIRGlobalID) = null
+        override fun getGlobalConstructors(): List<CIRFunctionID> = emptyList()
+        override fun getGlobalDestructors(): List<CIRFunctionID> = emptyList()
+        override fun close() = Unit
+    }
+
+    private fun stubFuncOp(functionTypeValue: MLIRTypeID): CIRFuncOp = CIRFuncOp(
+        symName = MLIRStringAttr("stub", null),
+        globalVisibility = CIRVisibilityAttr(CIRVisibilityKind.Default),
+        functionType = org.seqra.ir.api.cir.cfg.MLIRTypeAttr(functionTypeValue),
+        builtin = null,
+        coroutine = null,
+        lambda = null,
+        noProto = null,
+        dsolocal = null,
+        linkage = CIRGlobalLinkageKind.ExternalLinkage,
+        callingConv = CIRCallingConv.C,
+        extraAttrs = extraAttrs,
+        symVisibility = null,
+        comdat = null,
+        argAttrs = null,
+        resAttrs = null,
+        aliasee = null,
+        globalCtor = null,
+        globalDtor = null,
+        annotations = null,
+        ast = null,
+    )
+
+    private inner class StubFunction(
+        override val id: CIRFunctionID,
+        override val classpath: CIRClasspath,
+        override val parameters: List<CIRFunctionParameter>,
+        override val returnType: MLIRTypeID,
+        override val blocks: CIRBlockList,
+        private val funcOp: CIRFuncOp,
+        override val allInstructions: List<CIRInst>,
+    ) : CIRFunction {
+        override val info: CIRFuncOp get() = funcOp
+        override val assignInstByLhv: Map<MLIRValue, CIRAssignInst>
+            get() = allInstructions.filterIsInstance<CIRAssignInst>().associateBy { it.lhv }
+
+        override fun <T> withIRNode(body: (ByteArray?) -> T): T = body(null)
+        override fun flowGraph() = error("unused")
+
+        fun withInstructions(instructions: List<CIRInst>): StubFunction =
+            StubFunction(id, classpath, parameters, returnType, blocks, funcOp, instructions)
+    }
+
+    private class FixedValueResolver(
+        private val value: MLIRValue,
+    ) : org.seqra.dataflow.configuration.core.PositionResolver<Maybe<MLIRValue>> {
+        override fun resolve(position: org.seqra.dataflow.configuration.core.Position): Maybe<MLIRValue> = Maybe.some(value)
+    }
+
+    private fun emptyFunction(cp: CIRClasspath): StubFunction = StubFunction(
+        id = CIRFunctionID(moduleId, "f"),
+        classpath = cp,
+        parameters = emptyList(),
+        returnType = voidTy,
+        blocks = CIRBlockList(emptyList()),
+        funcOp = stubFuncOp(voidTy),
+        allInstructions = emptyList(),
+    )
+
+    private fun rewriter(method: CIRFunction, value: MLIRValue, cp: CIRClasspath = StubClasspath()) =
+        CIRMarkAwareConditionRewriter(
+            positionResolver = FixedValueResolver(value),
+            typeChecker = CIRFactTypeChecker(cp),
+            method = method,
+        )
+
+    @Test
+    fun `rewrite folds evaluated atoms and preserves mark literals`() {
+        val cp = StubClasspath()
+        val fn = emptyFunction(cp)
+        val lhv = MLIROpValue(intTy, MLIROpID(0), 0L)
+        val inst = CIRAssignInst(
+            location = CIRInstLocation(fn, 0, MLIRUnknownLoc),
+            id = MLIROpID(0),
+            lhv = lhv,
+            rhv = CIRConstantOpExpr(CIRIntAttr(null, BigInteger.valueOf(42)), intTy),
+        )
+        val fnWithInst = fn.withInstructions(listOf(inst))
+        val markCondition = ContainsMark(Argument(0), TaintMark("source"))
+
+        val result = rewriter(fnWithInst, lhv, cp).rewrite(
+            And(
+                listOf(
+                    ConstantEq(Argument(0), ConstantIntValue(42)),
+                    markCondition,
+                )
+            )
+        )
+
+        assertFalse(result.isTrue)
+        assertFalse(result.isFalse)
+        assertEquals(CIRMarkAwareConditionExpr.Literal(markCondition, negated = false), result.expr)
+    }
+
+    @Test
+    fun `rewrite negates contains mark literals under not`() {
+        val cp = StubClasspath()
+        val fn = emptyFunction(cp)
+        val lhv = MLIROpValue(intTy, MLIROpID(1), 0L)
+        val markCondition = ContainsMark(Argument(0), TaintMark("source"))
+
+        val result = rewriter(fn, lhv, cp).rewrite(Not(markCondition))
+
+        assertFalse(result.isTrue)
+        assertFalse(result.isFalse)
+        assertEquals(CIRMarkAwareConditionExpr.Literal(markCondition, negated = true), result.expr)
+    }
+
+    @Test
+    fun `rewrite short circuits and to false when one atom is false`() {
+        val cp = StubClasspath()
+        val fn = emptyFunction(cp)
+        val lhv = MLIROpValue(intTy, MLIROpID(2), 0L)
+        val inst = CIRAssignInst(
+            location = CIRInstLocation(fn, 0, MLIRUnknownLoc),
+            id = MLIROpID(2),
+            lhv = lhv,
+            rhv = CIRConstantOpExpr(CIRIntAttr(null, BigInteger.valueOf(42)), intTy),
+        )
+        val fnWithInst = fn.withInstructions(listOf(inst))
+
+        val result = rewriter(fnWithInst, lhv, cp).rewrite(
+            And(
+                listOf(
+                    ConstantEq(Argument(0), ConstantIntValue(7)),
+                    ContainsMark(Argument(0), TaintMark("source")),
+                )
+            )
+        )
+
+        assertTrue(result.isFalse)
+    }
+}
