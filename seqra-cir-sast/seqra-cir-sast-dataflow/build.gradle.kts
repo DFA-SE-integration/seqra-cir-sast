84c784832786129ed0533dabd94875121f241518
dmittrey <dmitryzubakhin@mail.ru>
2026-04-28 02:38:54 +0300

    seqra-cir-sast-dataflow: add submodule

---
+++ seqra-cir-sast/seqra-cir-sast-dataflow/build.gradle.kts
@@ -0,0 +1,40 @@
+import SeqraConfigurationDependency.seqraRulesCore
+import SeqraIrDependency.seqra_ir_api_cir
+import SeqraIrDependency.seqra_ir_api_storage
+import SeqraIrDependency.seqra_ir_core_cir
+import SeqraIrDependency.seqra_ir_storage
+import SeqraUtilDependency.seqraUtilCir
+import org.jetbrains.kotlin.konan.properties.loadProperties
+import org.seqra.common.JunitDependencies
+import org.seqra.common.KotlinDependency
+
+val rootProperties = layout.projectDirectory.file("../gradle.properties").asFile.absolutePath.let { loadProperties(it) }
+val cirDataflowVersion = rootProperties.getProperty("seqraBuildVersion")
+
+plugins {
+    id("kotlin-conventions")
+    kotlinSerialization()
+}
+
+dependencies {
+    implementation(seqraRulesCore)
+    implementation(seqraUtilCir)
+
+    implementation(seqra_ir_api_cir)
+    implementation(seqra_ir_core_cir)
+    implementation(seqra_ir_api_storage)
+    implementation(seqra_ir_storage)
+
+    implementation("org.seqra.seqra-dataflow-core:seqra-dataflow:${cirDataflowVersion}")
+    implementation("org.seqra.seqra-dataflow-core:seqra-cir-dataflow:${cirDataflowVersion}")
+
+    implementation(KotlinDependency.Libs.kotlin_logging)
+
+    testImplementation(JunitDependencies.Libs.junit_jupiter_params)
+    testImplementation(Libs.mockk)
+    testRuntimeOnly(Libs.logback)
+}
+
+tasks.withType<Test> {
+    jvmArgs = listOf("-Xmx4g")
+}
