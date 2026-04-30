45f566572c3f51eafbccef8a65bfcf5d588b0b4b
dmittrey <dmitryzubahin@gmail.com>
2026-05-01 01:07:20 +0300

    seqra-cir-dataflow: support preconditions

---
+++ seqra-dataflow-core/seqra-cir-dataflow/src/main/kotlin/org/seqra/dataflow/cir/ap/ifds/analysis/CIRMethodCallPrecondition.kt
@@ -0,0 +1,282 @@
+package org.seqra.dataflow.cir.ap.ifds.analysis
+
+import org.seqra.dataflow.ap.ifds.AccessPathBase
+import org.seqra.dataflow.ap.ifds.ExclusionSet
+import org.seqra.dataflow.ap.ifds.FinalAccessor
+import org.seqra.dataflow.ap.ifds.TaintMarkAccessor
+import org.seqra.dataflow.ap.ifds.access.ApManager
+import org.seqra.dataflow.ap.ifds.access.InitialFactAp
+import org.seqra.dataflow.ap.ifds.analysis.MethodCallFactMapper
+import org.seqra.dataflow.ap.ifds.trace.MethodCallPrecondition
+import org.seqra.dataflow.ap.ifds.trace.MethodCallPrecondition.CallPrecondition
+import org.seqra.dataflow.ap.ifds.trace.MethodCallPrecondition.CallPreconditionFact
+import org.seqra.dataflow.ap.ifds.trace.MethodCallPrecondition.PassRuleConditionFacts
+import org.seqra.dataflow.ap.ifds.trace.MethodCallPrecondition.PreconditionFactsForInitialFact
+import org.seqra.dataflow.ap.ifds.trace.TaintRulePrecondition
+import org.seqra.dataflow.ap.ifds.trace.TaintRulePrecondition.PassRuleCondition
+import org.seqra.dataflow.cir.ap.ifds.CIRMarkAwareConditionExpr
+import org.seqra.dataflow.cir.ap.ifds.CIRMarkAwareConditionRewriter
+import org.seqra.dataflow.cir.ap.ifds.CIRMethodCallFactMapper
+import org.seqra.dataflow.cir.ap.ifds.removeTrueLiterals
+import org.seqra.dataflow.cir.ap.ifds.CallPositionToCIRValueResolver
+import org.seqra.dataflow.cir.ap.ifds.MethodFlowFunctionUtils
+import org.seqra.dataflow.cir.ap.ifds.taint.CIRTaintRulesProvider
+import org.seqra.dataflow.cir.ap.ifds.taint.InitialFactReader
+import org.seqra.dataflow.cir.ap.ifds.taint.PositionAccess
+import org.seqra.dataflow.cir.ap.ifds.taint.TaintPassActionPreconditionEvaluator
+import org.seqra.dataflow.cir.ap.ifds.taint.TaintSourceActionPreconditionEvaluator
+import org.seqra.dataflow.cir.ap.ifds.taint.mkInitialAccessPath
+import org.seqra.dataflow.cir.ap.ifds.taint.resolveAp
+import org.seqra.dataflow.configuration.core.ContainsMark
+import org.seqra.dataflow.configuration.core.CopyAllMarks
+import org.seqra.dataflow.configuration.core.CopyMark
+import org.seqra.dataflow.configuration.core.TaintMark
+import org.seqra.dataflow.util.cartesianProductMapTo
+import org.seqra.ir.api.cir.cfg.CIRDirectCall
+import org.seqra.ir.api.cir.cfg.CIRInst
+import org.seqra.ir.api.cir.cfg.MLIRValue
+import org.seqra.util.Maybe
+import org.seqra.util.maybeFlatMap
+
+class CIRMethodCallPrecondition(
+    private val apManager: ApManager,
+    private val analysisContext: CIRMethodAnalysisContext,
+    private val returnValue: MLIRValue?,
+    private val callExpr: CIRDirectCall,
+    private val statement: CIRInst,
+) : MethodCallPrecondition {
+    private val methodCallFactMapper: MethodCallFactMapper get() = analysisContext.methodCallFactMapper
+
+    private val cIRValueResolver = CallPositionToCIRValueResolver(callExpr, returnValue)
+    private val method = callExpr.calleeRef?.function
+
+    private val taintConfig get() = analysisContext.taint.taintConfig as CIRTaintRulesProvider
+
+    override fun factPrecondition(fact: InitialFactAp): CallPrecondition {
+        val results = mutableListOf<PreconditionFactsForInitialFact>()
+
+        preconditionForFact(fact)?.let {
+            results.add(PreconditionFactsForInitialFact(fact, it))
+        }
+
+        analysisContext.aliasAnalysis?.forEachPossibleAliasAtStatement(statement, fact) { aliasedFact ->
+            preconditionForFact(aliasedFact)?.let {
+                results.add(PreconditionFactsForInitialFact(aliasedFact, it))
+            }
+        }
+
+        return if (results.isEmpty()) {
+            CallPrecondition.Unchanged
+        } else {
+            CallPrecondition.Facts(results)
+        }
+    }
+
+    private fun preconditionForFact(fact: InitialFactAp): List<CallPreconditionFact>? {
+        if (!CIRMethodCallFactMapper.factIsRelevantToMethodCall(returnValue, callExpr, fact)) {
+            return null
+        }
+
+        val preconditions = mutableListOf<CallPreconditionFact>()
+
+        if (returnValue != null) {
+            val returnValueBase = MethodFlowFunctionUtils.accessPathBase(returnValue)
+            if (returnValueBase == fact.base) {
+                preconditions.preconditionForFact(fact, AccessPathBase.Return)
+            }
+        }
+
+        val callee = method
+        if (callee != null) {
+            CIRMethodCallFactMapper.mapMethodCallToStartFlowFact(callee, callExpr, fact) { callerFact, startFactBase ->
+                preconditions.preconditionForFact(callerFact, startFactBase)
+            }
+        }
+
+        return preconditions
+    }
+
+    private fun MutableList<CallPreconditionFact>.preconditionForFact(fact: InitialFactAp, startBase: AccessPathBase) {
+        val rulePreconditions = mutableListOf<TaintRulePrecondition>()
+        rulePreconditions.factSourceRulePrecondition(fact, startBase)
+        rulePreconditions.factPassRulePrecondition(fact, startBase)
+
+        rulePreconditions.mapTo(this) { CallPreconditionFact.CallToReturnTaintRule(it) }
+
+        this += CallPreconditionFact.CallToStart(fact, startBase)
+    }
+
+    sealed interface CIRPassRuleCondition : PassRuleCondition {
+        data class Expr(val expr: CIRMarkAwareConditionExpr) : CIRPassRuleCondition
+        data class Fact(val fact: InitialFactAp) : CIRPassRuleCondition
+        data class FactWithExpr(val fact: InitialFactAp, val expr: CIRMarkAwareConditionExpr) : CIRPassRuleCondition
+    }
+
+    private fun MutableList<TaintRulePrecondition>.factSourceRulePrecondition(
+        fact: InitialFactAp,
+        startBase: AccessPathBase,
+    ) {
+        val callee = method ?: return
+
+        val entryFactReader = InitialFactReader(fact.rebase(startBase), apManager)
+        val sourcePreconditionEvaluator = TaintSourceActionPreconditionEvaluator(
+            entryFactReader, analysisContext.factTypeChecker,
+            callee.classpath.findTypeOrNull(callee.returnType)
+        )
+
+        val conditionRewriter = CIRMarkAwareConditionRewriter(
+            cIRValueResolver,
+            statement.method
+        )
+
+        for (rule in taintConfig.sourceRulesForMethod(callee, statement)) {
+            val assignedMarks = rule.actionsAfter.maybeFlatMap {
+                sourcePreconditionEvaluator.evaluate(rule, it)
+            }
+            if (assignedMarks.isNone) continue
+
+            val sourceActions = assignedMarks.getOrThrow().mapTo(hashSetOf()) { it.second }
+
+            val simplifiedCondition = conditionRewriter.rewrite(rule.condition)
+
+            val simplifiedExpr = when {
+                simplifiedCondition.isFalse -> continue
+                simplifiedCondition.isTrue -> null
+                else -> simplifiedCondition.expr
+            }
+
+            // We always treat negated mark condition as satisfied
+            val exprWithoutNegations = simplifiedExpr?.removeNegated()
+            if (exprWithoutNegations == null) {
+                this += TaintRulePrecondition.Source(rule, sourceActions)
+                continue
+            }
+
+            this += TaintRulePrecondition.Pass(
+                rule, sourceActions,
+                CIRPassRuleCondition.Expr(exprWithoutNegations)
+            )
+        }
+    }
+
+    private fun MutableList<TaintRulePrecondition>.factPassRulePrecondition(
+        fact: InitialFactAp,
+        startBase: AccessPathBase,
+    ) {
+        val callee = method ?: return
+        val passRules = taintConfig.passTroughRulesForMethod(callee, statement).toList()
+        if (passRules.isEmpty()) return
+
+        val entryFactReader = InitialFactReader(fact.rebase(startBase), apManager)
+        val rulePreconditionEvaluator = TaintPassActionPreconditionEvaluator(
+            entryFactReader, analysisContext.factTypeChecker,
+            callee.classpath.findTypeOrNull(callee.returnType)
+        )
+
+        val conditionRewriter = CIRMarkAwareConditionRewriter(
+            cIRValueResolver,
+            statement.method
+        )
+
+        for (rule in passRules) {
+            val actions = rule.actionsAfter.maybeFlatMap {
+                when (it) {
+                    is CopyMark -> rulePreconditionEvaluator.evaluate(rule, it)
+                    is CopyAllMarks -> rulePreconditionEvaluator.evaluate(rule, it)
+                    else -> Maybe.none()
+                }
+            }
+            if (actions.isNone) continue
+
+            val passActions = actions.getOrThrow()
+
+            val simplifiedCondition = conditionRewriter.rewrite(rule.condition)
+
+            val simplifiedExpr = when {
+                simplifiedCondition.isFalse -> continue
+                simplifiedCondition.isTrue -> null
+                else -> simplifiedCondition.expr
+            }
+
+            // We always treat negated mark condition as satisfied
+            val exprWithoutNegations = simplifiedExpr?.removeNegated()
+
+            val mappedAction = passActions.flatMap { (action, fact) ->
+                methodCallFactMapper.mapMethodExitToReturnFlowFact(statement, fact).map { action to it }
+            }
+
+            mappedAction.mapTo(this) { (action, fact) ->
+                val cond = if (exprWithoutNegations == null) {
+                    CIRPassRuleCondition.Fact(fact)
+                } else {
+                    CIRPassRuleCondition.FactWithExpr(fact, exprWithoutNegations)
+                }
+                TaintRulePrecondition.Pass(rule, setOf(action), cond)
+            }
+        }
+    }
+
+    private fun CIRMarkAwareConditionExpr.removeNegated() = removeTrueLiterals { it.negated }
+
+    override fun resolvePassRuleCondition(precondition: PassRuleCondition): List<PassRuleConditionFacts> {
+        precondition as CIRPassRuleCondition
+
+        return when (precondition) {
+            is CIRPassRuleCondition.Fact -> {
+                listOf(PassRuleConditionFacts(listOf(precondition.fact)))
+            }
+
+            is CIRPassRuleCondition.Expr -> {
+                precondition.expr.preconditionDnf().map { PassRuleConditionFacts(it.facts.toList()) }
+            }
+
+            is CIRPassRuleCondition.FactWithExpr -> {
+                precondition.expr.preconditionDnf().map {
+                    val allFacts = it.facts + precondition.fact
+                    PassRuleConditionFacts(allFacts.toList())
+                }
+            }
+        }
+    }
+
+    private fun ContainsMark.preconditionFact(): InitialFactAp {
+        return createPositionWithTaintMark(position.resolveAp(), mark)
+    }
+
+    private fun createPositionWithTaintMark(position: PositionAccess, mark: TaintMark): InitialFactAp {
+        val positionWithMark = PositionAccess.Complex(position, TaintMarkAccessor(mark.name))
+        val finalPositionWithMark = PositionAccess.Complex(positionWithMark, FinalAccessor)
+        return createPosition(finalPositionWithMark)
+    }
+
+    private fun createPosition(position: PositionAccess): InitialFactAp {
+        var normalizedPosition = position
+        if (position is PositionAccess.Complex && position.accessor is FinalAccessor) {
+            // mkInitialAccessPath starts with final ap
+            normalizedPosition = position.base
+        }
+        return apManager.mkInitialAccessPath(normalizedPosition, ExclusionSet.Universe)
+    }
+
+    private data class PreconditionCube(val facts: Set<InitialFactAp>)
+
+    private fun CIRMarkAwareConditionExpr.preconditionDnf(): List<PreconditionCube> = when (this) {
+        is CIRMarkAwareConditionExpr.Literal -> {
+            val preconditionFact = condition.preconditionFact()
+            val mappedFacts = methodCallFactMapper.mapMethodExitToReturnFlowFact(statement, preconditionFact)
+            mappedFacts.map { PreconditionCube(setOf(it)) }
+        }
+
+        is CIRMarkAwareConditionExpr.Or -> args.flatMap { it.preconditionDnf() }
+        is CIRMarkAwareConditionExpr.And -> {
+            val result = mutableListOf<PreconditionCube>()
+            val cubeLists = args.map { it.preconditionDnf() }
+            cubeLists.cartesianProductMapTo { cubes ->
+                val facts = hashSetOf<InitialFactAp>()
+                cubes.flatMapTo(facts) { it.facts }
+                result += PreconditionCube(facts)
+            }
+            result
+        }
+    }
+}
