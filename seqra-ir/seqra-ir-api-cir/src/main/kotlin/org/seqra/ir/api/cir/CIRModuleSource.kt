9dd1908703b30b67aa04a9ccfc5763ca391b7a02
dmittrey <dmitryzubakhin@mail.ru>
2026-05-04 00:35:59 +0300

    fixup! fixup! seqra-ir-cir: support CIR dialect

--- seqra-ir/seqra-ir-api-cir/src/main/kotlin/org/seqra/ir/api/cir/CIRModuleSource.kt
+++ seqra-ir/seqra-ir-api-cir/src/main/kotlin/org/seqra/ir/api/cir/CIRModuleSource.kt
@@ -3,4 +3,8 @@ package org.seqra.ir.api.cir
 interface CIRModuleSource {
     val node: IRNode
     val location: RegisteredLocation
+
+    /** Serialized [org.seqra.ir.impl.grpc.Alias.CIRModuleAliasData] for this module, if present. */
+    val aliasData: ByteArray?
+        get() = null
 }
