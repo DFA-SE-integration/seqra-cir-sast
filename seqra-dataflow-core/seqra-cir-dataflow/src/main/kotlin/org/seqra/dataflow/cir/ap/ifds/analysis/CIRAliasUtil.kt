1bfe71ac7087511718ea1a0cb0d35bbc5292c9ba
dmittrey <dmitryzubakhin@mail.ru>
2026-05-04 00:46:50 +0300

    FIXME: CIRAliasUtil

--- seqra-dataflow-core/seqra-cir-dataflow/src/main/kotlin/org/seqra/dataflow/cir/ap/ifds/analysis/CIRAliasUtil.kt
+++ seqra-dataflow-core/seqra-cir-dataflow/src/main/kotlin/org/seqra/dataflow/cir/ap/ifds/analysis/CIRAliasUtil.kt
@@ -4,10 +4,12 @@ import org.seqra.dataflow.ap.ifds.access.FinalFactAp
 import org.seqra.dataflow.cir.ap.ifds.CIRLocalAliasAnalysis
 import org.seqra.ir.api.cir.cfg.CIRInst
 
+@Suppress("UNUSED_PARAMETER")
 fun CIRLocalAliasAnalysis.forEachAliasAtStatement(statement: CIRInst, fact: FinalFactAp, body: (FinalFactAp) -> Unit) {
-//    val base = fact.base as? AccessPathBase.LocalVar ?: return
-//    val alias = findAlias(base, statement) ?: return
-//    if (alias.base is AccessPathBase.Constant) return
-//
-//    applyAlias(fact, alias, body)
+    val base = fact.base
+    val aliases = findAliases(base) ?: return
+    for (a in aliases) {
+        if (a == base) continue
+        body(fact.rebase(a))
+    }
 }
